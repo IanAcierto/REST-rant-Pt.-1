c1fdf97832a7407b2e876055db210ea16ed618d1
Ian Acierto <acierto_vincent@yahoo.com>
2022-07-11 14:02:31 -0700

    made error page

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 res.render('home')
 })
 app.get('*', (req,res) =>{
-  res.status(404).send('<h1>404 Page Not Found</h1>')
+  res.status(404).render('error404')
 })
 
 //LISTEN
---
+++ views/error404.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Def = require('./default')
+
+function error404(){
+  return(
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>oopsie, made a poopsie, i cant seem to find dis page!!! oowoo >_<</p>
+      </main>
+    </Def>
+  )
+}
+
+module.exports = error404;
